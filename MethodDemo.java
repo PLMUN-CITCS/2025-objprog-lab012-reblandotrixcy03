66377b2f945fc0b0afaec49b250ee52e5c983ce9
Trixcy Reblando <reblandotrixcyann_bscs@plmun.edu.ph>
2025-03-15 12:44:56 +0800

    Submitting OBJPROG Week 05 - Laboratory # 12

---
+++ MethodDemo.java
@@ -0,0 +1,15 @@
+public class MethodDemo {
+
+    public static void greet() {
+        System.out.println("Hello from the greet method!");
+    }
+
+    public static void printMessage(String message) {
+        System.out.println(message);
+    }
+
+    public static void main(String[] args) {
+        greet();
+        printMessage("This is my message!");
+    }
+}
